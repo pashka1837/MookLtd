ed92d89f28466d89de3706493acc46bc9ae8a018
pashka1837 <pashka1837@yandex.kz>
2024-04-13 15:36:57 +0500

    feat: serever worker added

--- src/Components/VideoOutput/VideoOutput.tsx
+++ src/Components/VideoOutput/VideoOutput.tsx
@@ -2,13 +2,7 @@ import testIMG from "../../assets/test.png";
 
 export default function VideoOutput({ width, height }: VideoOutT) {
   return (
-    <video
-      src="input_2.mp4"
-      poster={testIMG}
-      width={width}
-      height={height}
-      loop
-    >
+    <video src="" poster={testIMG} width={width} height={height} loop>
       Sorry, your browser doesn't support HTML5 video tag.
     </video>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf}"],
+      },
+      manifest: {
+        name: "MookLtd App",
+        short_name: "MookLtd",
+        description: "MookLtd",
+        theme_color: "#00cc99",
+        background_color: "#00cc99",
+        icons: [
+          {
+            src: "assets/images/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/images/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "assets/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
